761de74fc43e011fa1c58eae50ca95e0f6631533
Kristof <kristofosztopani@gmail.com>
2020-08-09 20:28:34 +0200

    added node_modules

--- run.cmd
+++ run.cmd
@@ -2,6 +2,6 @@
 cd Image-in-Image-release\node
 create_image.exe
 IF %ERRORLEVEL%==0 (cd .. && handler.exe)
-if %ERRORLEVEL%==0 (echo Your image is at: %CD%/node/result.png) ELSE (cd .. && echo Mission failed!)
+if %ERRORLEVEL%==0 (echo Your image is at: %CD%\result.png) ELSE (cd .. && echo Mission failed!)
 cd ..
 pause
